a03ebc78a9c2f3d59191899be725cd7859705df6
Aris A <aris.carter@hgs.hiddengeniusproject.org>
2020-12-08 23:49:22 -0800

    modified roi calculator

--- Baba's_money.py
+++ Baba's_money.py
@@ -1,3 +1,10 @@
+"""
+P = Principle (present value)
+r = interest rate
+n = how many paymwnts per year
+t = how many years
+pv = future value
+"""
 
 
 
@@ -6,13 +13,17 @@ r = float(input("Whats the Rate "))
 n = int(input('How many periods '))
 t = int(input("How many paymentsper period "))
 pv = p * (pow((1 + r/100/n),n*t))
-print(pv)
+
 
 def compound_interest(p,r,n,t):
     balance = p * (pow((1+ r/100/n),n*t))
     ci = balance - p
-    print('Balance at the end of the period ',round(balance,2))
-    print("Interest earned is ", round(ci,2))
+    print('\n Balance at the end of the period will be $',round(balance,2))
+    print("\n Interest earned is ", round(ci,2), 'during the',t,'year period.')
+    pct = ci/balance*100
+    print("This is a ",round(pct,2),'% return on investment')
+
+
 
 
 compound_interest(5000,10,12,5)
